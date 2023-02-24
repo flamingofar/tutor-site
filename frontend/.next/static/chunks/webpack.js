3eccf694d8f659011e960d732398528245161d49
malte <msk@charlietango.dk>
2023-02-24 12:59:13 +0100

    ...

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "360b604ede8d02fa"; }
+/******/ 		__webpack_require__.h = function() { return "d8aa04c660823186"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
