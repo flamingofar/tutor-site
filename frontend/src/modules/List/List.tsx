3e36897814e549dba93d64e1e2cddbc0badf2ed1
malte <msk@charlietango.dk>
2023-02-26 00:14:17 +0100

    fix

--- frontend/src/modules/List/List.tsx
+++ frontend/src/modules/List/List.tsx
@@ -1,19 +1,20 @@
+import React from "react";
 import ListElement from "../../components/ListElement/ListElement";
 import TitleAndDescription from "../../components/TitleAndDescription/TitleAndDescription";
 import { ListViewmodel } from "./ListViewmodel";
 
 const List = ({ entries, titleAndDescription }: ListViewmodel) => {
-	return (
-		<section className="px-20 space-y-4 ">
-			<TitleAndDescription {...titleAndDescription} />
-			<ul className="flex flex-col gap-4 ">
-				{entries &&
-					entries.map((entry, idx) => {
-						return <ListElement key={idx} {...entry} />;
-					})}
-			</ul>
-		</section>
-	);
+  return (
+    <section className="px-20 space-y-4 ">
+      <TitleAndDescription {...titleAndDescription} />
+      <ul className="flex flex-col gap-4 ">
+        {entries &&
+          entries.map((entry, idx) => {
+            return <ListElement key={idx} {...entry} />;
+          })}
+      </ul>
+    </section>
+  );
 };
 
 export default List;
