dca141e2422b269b3736e85ae82e7b371073928d
malte <msk@charlietango.dk>
2023-02-25 22:30:20 +0100

    Fixed button error

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import clsx from "clsx";
+import { ButtonViewmodel } from "./ButtonViewmodel";
 
-const Button = (props) => {
-	const { children, className } = props;
+const Button = ({ children, className }: ButtonViewmodel) => {
 	return (
 		<button className={clsx("bg-neutral-400 rounded-sm px-2 py-0.5", className)}>{children}</button>
 	);
