99cfd06872707f4e5158a6bc56b7f521dd918381
malte <msk@charlietango.dk>
2023-02-28 17:41:58 +0100

    colors for list

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,15 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"primary-list": "#AA81EE",
+				"list-1": "#FFD302",
+				"list-2": "#7487ED",
+				"list-3": "#A3ED68",
+				"list-4": "#E68CED",
+			},
+		},
 	},
 	plugins: [],
 };
